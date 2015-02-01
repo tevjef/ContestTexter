7251444e4f4ae447e5ab3e3c8e98954824a33515
tevjef <mastermindtj94@gmail.com>
2015-01-31 19:35:27 -0500

    Fixed bug in starting the alarmmanager

--- app/src/main/java/autotexter/tevinjeffrey/com/tacobellautotexter/MainActivity.java
+++ app/src/main/java/autotexter/tevinjeffrey/com/tacobellautotexter/MainActivity.java
@@ -7,16 +7,18 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
@@ -31,23 +33,27 @@ public class MainActivity extends Activity {
 
         alarmMgr = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, AlarmReceiver.class);
-        alarmIntent = PendingIntent.getBroadcast(this, 7868768, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmIntent = PendingIntent.getBroadcast(this, 1234, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
+        Toast.makeText(getApplicationContext(), "Sending text at 8:30am every day", Toast.LENGTH_LONG).show();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         // Set the alarm to start at 8:30 a.m.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+        Log.d("AM", "Calendar set to: " + dateFormat.format(calendar.getTime()));
 
-        // With setInexactRepeating(), you have to use one of the AlarmManager interval
-        // constants--in this case, AlarmManager.INTERVAL_DAY.
-
-        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, System.currentTimeMillis() + 3000,
-                10*1000 , alarmIntent);
-
-        Toast.makeText(this, "Alarm set", Toast.LENGTH_LONG).show();
+        alarmMgr.cancel(alarmIntent);
+        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, alarmIntent);
+        Date d = new Date(calendar.getTimeInMillis());
+        Log.d("AM", "Alarm Set to: " + dateFormat.format(d));
 
         finish();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
